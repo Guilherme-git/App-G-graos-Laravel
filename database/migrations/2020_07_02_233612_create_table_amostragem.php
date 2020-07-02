50ec869cf897b5cf0dd0e861705469f81874064d
Guilherme Ataides <guilhermedacatia132@gmail.com>
2020-07-02 20:43:58 -0300

    Configurando mostrar usuario

---
+++ database/migrations/2020_07_02_233612_create_table_amostragem.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAmostragem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('amostragem', function (Blueprint $table) {
+            $table->increments('id_amos');
+            $table->date('data');
+            $table->string('estado');
+            $table->string('pesoAmostragem');
+            $table->string('placaCaminhaoAmostragem');
+            $table->string('tipoGrao');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('amostragem');
+    }
+}
